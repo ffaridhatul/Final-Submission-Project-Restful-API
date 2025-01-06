4b0c0b552bca8b3e07509cb18968e45137513b03
ffaridhatul <faridhatulzikri11@gmail.com>
2025-01-06 11:38:57 +0700

    Final aplikasi Rest API (WMS)

---
+++ src/main/java/com/warungmakansamudra/ChallengeRestfulApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.warungmakansamudra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeRestfulApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeRestfulApiJavaApplication.class, args);
+	}
+
+}
